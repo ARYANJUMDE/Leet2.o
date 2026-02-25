4f26b4ed50e0599bcfd2f932309857fdbddd8d51
Aryan <jumdearyan7@gmail.com>
2026-02-25 12:30:22 +0530

    Time: 74 ms (76.61%), Space: 48 MB (30.6%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def partition(self, s):
+        res = []
+        
+        def backtrack(start, path):
+            if start == len(s):
+                res.append(path[:])
+                return
+            
+            
+            for end in range(start, len(s)):
+                substring = s[start:end+1]
+                
+            
+                if substring == substring[::-1]:
+                    path.append(substring)      
+                    backtrack(end + 1, path)  
+                    path.pop()               
+        
+        backtrack(0, [])
+        return res
+
+
+        
