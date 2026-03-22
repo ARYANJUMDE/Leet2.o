5bd084a17dbf9767ad0f7b3095bc921fd5602827
Aryan <jumdearyan7@gmail.com>
2026-03-22 11:26:45 +0530

    Time: 681 ms (27.87%), Space: 0B (100%) - LeetHub

---
+++ 3570-find-books-with-no-available-copies/3570-find-books-with-no-available-copies.sql
@@ -0,0 +1,21 @@
+/* Write your PL/SQL query statement below */
+SELECT 
+    b.book_id,
+    b.title,
+    b.author,
+    b.genre,
+    b.publication_year,
+    COUNT(*) AS current_borrowers
+FROM library_books b
+JOIN borrowing_records r
+    ON b.book_id = r.book_id
+WHERE r.return_date IS NULL
+GROUP BY 
+    b.book_id,
+    b.title,
+    b.author,
+    b.genre,
+    b.publication_year,
+    b.total_copies
+HAVING COUNT(*) = b.total_copies
+ORDER BY current_borrowers DESC, b.title;
