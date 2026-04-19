d7881833afded2cb3fe225e7c7efd539484ddce9
Aryan <jumdearyan7@gmail.com>
2026-04-19 10:17:28 +0530

    Time: 0 ms (100%), Space: 12.3 MB (63.24%) - LeetHub

---
+++ 3151-special-array-i/3151-special-array-i.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isArraySpecial(self, nums):
+        if(len(nums)==1):
+            return(True)
+        else:
+            for i in range(len(nums)-1):
+                if (nums[i]%2==0 and nums[i+1]%2==0) or (nums[i]%2!=0 and nums[i+1]%2!=0):
+                    return(False)
+            else:
+                return(True)
+            
+
+        
