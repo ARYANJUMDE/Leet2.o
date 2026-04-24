4a9922787835ce0c4f4a7819d01528caeebf769d
Aryan <jumdearyan7@gmail.com>
2026-04-24 12:09:53 +0530

    Time: 3 ms (38.61%), Space: 13.1 MB (90.51%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -4,30 +4,25 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
-
 class Solution(object):
     def levelOrder(self, root):
         if root is None:
             return []
+        queue=deque()
+        result=[]
         
-        result = []
-        q = deque([root])
-        
-        while q:
-            level = []
-            size = len(q)
-            
-            for i in range(size):
-                node = q.popleft()
+        queue.append(root)
+        while queue:
+            level_size=len(queue)
+            level=[]
+            for i in range(level_size):
+                node=queue.popleft()
                 level.append(node.val)
-                
                 if node.left:
-                    q.append(node.left)
+                    queue.append(node.left)
                 if node.right:
-                    q.append(node.right)
-            
+                    queue.append(node.right)
             result.append(level)
-        
         return result
+    
         
