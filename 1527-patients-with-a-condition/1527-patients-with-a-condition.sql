5cb7fd11bd2e529ffd92434d6e6726713fb32ba3
Aryan <jumdearyan7@gmail.com>
2026-07-28 17:44:17 +0530

    Time: 456 ms (28.43%), Space: 0B (100%) - LeetHub

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -1,2 +1,2 @@
-/* Write your PL/SQL query statement below */
-SELECT patient_id,patient_name,conditions FROM Patients WHERE conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%';
+# Write your MySQL query statement below
+select patient_id, patient_name,conditions from Patients where conditions like 'DIAB1%' or conditions like "% DIAB1%";
