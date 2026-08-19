83a58ca10dfaa45996d6457f84829179b11c1ac7
Aryan <jumdearyan7@gmail.com>
2026-08-19 22:37:57 +0530

    Time: 18 ms (60.87%), Space: 12.6 MB (20.79%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,17 +1,31 @@
 class Solution(object):
     def combinationSum(self, candidates, target):
         result=[]
-        def backtrack(start, path, total):
-            if total == target:
-                result.append(path[:])
+        def solve(a,i,target):
+            if target==0:
+                result.append(a[:])
                 return
-            if total > target:
+            if target<0 or i==len(candidates):
                 return
-            for i in range(start, len(candidates)):
-                
-                path.append(candidates[i])
-                backtrack(i, path, total + candidates[i])
-                path.pop()
-        backtrack(0, [], 0)
+            else:
+                a.append(candidates[i])
+                solve(a,i,target-candidates[i])
+                a.pop()
+                solve(a,i+1,target)
+        solve([],0,target)
         return result
+        # result=[]
+        # def backtrack(start, path, total):
+        #     if total == target:
+        #         result.append(path[:])
+        #         return
+        #     if total > target:
+        #         return
+        #     for i in range(start, len(candidates)):
+                
+        #         path.append(candidates[i])
+        #         backtrack(i, path, total + candidates[i])
+        #         path.pop()
+        # backtrack(0, [], 0)
+        # return result
         
