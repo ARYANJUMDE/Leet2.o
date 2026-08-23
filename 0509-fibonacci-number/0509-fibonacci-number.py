18c691c11e6b80c9575daa87fcd1f9ec1a3b7e7e
Aryan <jumdearyan7@gmail.com>
2026-08-23 16:07:49 +0530

    Time: 12 ms (84.07%), Space: 12.3 MB (89.32%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -7,15 +7,23 @@ class Solution(object):
         #     l.append(l[i-1]+l[i-2])
         
         # return l[len(l)-1]+l[len(l)-2]
-        if n==0:
-            return 0
-        if n==1:
-            return 1
+        # if n==0:
+        #     return 0
+        # if n==1:
+        #     return 1
+        # dp=[-1]*(n+1)
+        # dp[0]=0
+        # dp[1]=1
+
+        # for i in range(2,len(dp)):
+        #     dp[i]=dp[i-1]+dp[i-2]
+        # return dp[n]
+        if n==0 or n==1:
+            return n
         dp=[-1]*(n+1)
         dp[0]=0
         dp[1]=1
-
-        for i in range(2,len(dp)):
+        for i in range(2,n+1):
             dp[i]=dp[i-1]+dp[i-2]
         return dp[n]
 
