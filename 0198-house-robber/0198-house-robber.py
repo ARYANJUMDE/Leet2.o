0528b126191463859267facf513b2d83faec7483
Aryan <jumdearyan7@gmail.com>
2026-08-27 21:57:42 +0530

    Time: 3 ms (6.96%), Space: 12.4 MB (59.01%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,11 +1,39 @@
 class Solution(object):
     def rob(self, nums):
-        prev1 = 0
-        prev2 = 0
+        dp=[-1]*len(nums)
+        def robbing(i):
+            if i>=len(nums):
+                return 0
+            if dp[i]!=-1:
+                return dp[i]
+            take=nums[i]+robbing(i+2)
+            skip=robbing(i+1)
+            dp[i]=max(take,skip)
+            return dp[i]
+        return robbing(0)
+        # dp=[-1]*len(nums)
+        # for i in range(len(nums)):
         
-        for num in nums:
-            temp = max(prev1, prev2 + num)
-            prev2 = prev1
-            prev1 = temp
+        # Normal recusion code
+        # x=[]
+        # def robbing(sum1,i):
+        #     if i>=len(nums):
+        #         x.append(sum1)
+        #         return
+        #     else:
+        #         sum1=sum1+nums[i]
+        #         robbing(sum1,i+2)
+        #         sum1=sum1-nums[i]
+        #         robbing(sum1,i+1)
+        # t=robbing(0,0)
+        # return (max(x))    
+
+        # prev1 = 0
+        # prev2 = 0
         
-        return prev1
+        # for num in nums:
+        #     temp = max(prev1, prev2 + num)
+        #     prev2 = prev1
+        #     prev1 = temp
+        
+        # return prev1
