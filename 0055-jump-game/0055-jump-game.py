c621e9ba3ba96ed366c8f1260e6ea8344456f579
Aryan <jumdearyan7@gmail.com>
2026-09-20 14:54:00 +0530

    Time: 37 ms (49.02%), Space: 13.4 MB (30.36%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,15 +1,26 @@
 class Solution(object):
     def canJump(self, nums):
-        reach=0
-        #This stores the farthest index you can reach so far
+        # reach=0
+        # #This stores the farthest index you can reach so far
         #Initially, you're at index 0, so reach = 0
-        for i in range(len(nums)):
-            #If current index i is greater than what you can reach,
+        # for i in range(len(nums)):
+        #     #If current index i is greater than what you can reach,
             #it means:
             #you can’t even get to this position
-            if i>reach:
-                return(False)
-            else:
-                reach=max(reach,i+nums[i])
-        else:
-            return(True)
+        #     if i>reach:
+        #         return(False)
+        #     else:
+        #         reach=max(reach,i+nums[i])
+        # else:
+        #     return(True)
+
+        max_index=0
+        for i in range(len(nums)):
+            if i>max_index:
+                return False
+            jump=i+nums[i]
+            if jump>max_index:
+                max_index=jump
+            if max_index>=len(nums)-1:
+                return True
+            
