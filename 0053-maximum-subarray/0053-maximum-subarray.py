4883260762fcd207834297f480099743d08b4cc7
Aryan <jumdearyan7@gmail.com>
2026-09-22 18:03:43 +0530

    Time: 63 ms (73.76%), Space: 21.8 MB (18.65%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -15,15 +15,16 @@ class Solution(object):
         #         subarray([],i+1)
         # subarray([],0)
         # return max(result)
-        sum1=0
-        max1=nums[0]
-        for i in range(len(nums)):
-            sum1=sum1+nums[i]
-            if max1<sum1:
-                max1=sum1
-            if sum1<0:
-                sum1=0
-        return max1
+        # sum1=0
+        # max1=nums[0]
+        # for i in range(len(nums)):
+        #     sum1=sum1+nums[i]
+        #     if max1<sum1:
+        #         max1=sum1
+        #     if sum1<0:
+        #         sum1=0
+        # return max1
+        
             
 
         # max_sum = nums[0]
@@ -35,4 +36,34 @@ class Solution(object):
         #     max_sum = max(max_sum, curr_sum)
         
         # return max_sum
+
+
+        i=0
+        j=0
+        sum1=0
+        max_sum=float('-inf')
+        while j<len(nums):
+            if sum1>=0:
+                sum1=sum1+nums[j]
+                if sum1>max_sum:
+                    max_sum=sum1
+                j=j+1
+            else:
+                while sum1<0:
+                    sum1=sum1-nums[i]
+                    i=i+1
+                    if sum1>max_sum:
+                        max_sum=sum1
+        if max_sum==0:
+            if 0 in nums:
+                return max_sum
+        if max_sum==0:
+            if 0 not in nums:
+                return max(nums)
+        return max_sum
+        
+
+                    
+    
+
 
